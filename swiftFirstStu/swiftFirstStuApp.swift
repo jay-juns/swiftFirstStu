67578dd8e109f37ad7fa60f3d8ae05b9dd1ad612
jay-juns <necrozerg2@naver.com>
2024-04-03 18:48:57 +0900

    Initial Commit

---
+++ swiftFirstStu/swiftFirstStuApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftFirstStuApp.swift
+//  swiftFirstStu
+//
+//  Created by junseok Kim on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftFirstStuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
